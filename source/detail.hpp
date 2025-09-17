fdfe3dac7bc9b5fd8440f5ac61ee1ab9b8cc4393
WMSF111 <2364169122@qq.com>
2025-09-17 23:38:02 +0800

    33节完成宏定义

--- source/detail.hpp
+++ source/detail.hpp
@@ -11,8 +11,12 @@
 #include<string>
 #include <sstream>
 #include<jsoncpp/json/json.h>
+#include <chrono>
+#include <random>
+#include <atomic>
+#include <iomanip>
 
-namespace bitspace
+namespace bitrbc
 {
 
 #define LDBG 0
@@ -75,5 +79,36 @@ namespace bitspace
             }
             return true;
         }
+    };
+
+    class UUID
+    {
+        public:
+            static std::string uuid()
+            {
+                std::stringstream ss; // 构建返回的string
+                std::random_device rd;// 1.构造一个机器随机数对象（种子）
+                std::mt19937 generator(rd());// 2.以硬件随机数为种子构造伪随机对象(将rd作为)
+                std::uniform_int_distribution<int> distribution(0, 255); // 3.构造限定数据范围的对象
+                for(int i = 0; i < 8; i++) // 4. ⽣成8个2位的16进制随机数，按需求排序
+                {
+                    if(i == 4 || i == 6) ss << "-";
+                    // setw: 设置位数 setfill: 不满2位时用0填充 std::hex: 16进制显示
+                    ss << std::setw(2) << std::setfill('0') << std::hex << distribution(generator);
+                }
+                ss << "-";
+                // 5.定义⼀个8字节序号（一字节8比特位），逐字节组织成为16进制数字字符的字符串 // 0000 0001
+                static std::atomic<size_t> seq(1); // 定义一个线程安全的静态计数器seq，它会随着每次调用而递增
+                size_t cur = seq.fetch_add(1); // 获取当前值并递增seq
+                for (int i = 7; i >= 0; i--) { // 将一个size_t类型的变量（cur）转化为8个16进制字符
+                    if (i == 5) ss << "-";// 从最高位到最低位的8个16进制
+                    //0000 0001 一字节8比特位
+                    //0000 0000 & 1111 1111 右移7位
+                    //00 输出为16字节2位
+                    //0000-000000000001
+                    ss << std::setw(2) << std::setfill('0') << std::hex << ((cur >> (i*8)) & 0xFF);
+                }
+                return ss.str();
+            }
     };
 }
---
+++ source/fieIds.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<unordered_map>
+
+
+namespace bitrpc{
+    #define KEY_METHOD "method"
+    #define KEY_PARAMS "parameters"
+    #define KEY_TOPIC_KEY "topic_key"
+    #define KEY_TOPIC_MSG "topic_msg"
+    #define KEY_OPTYPE "optype"
+    #define KEY_HOST "host"
+    #define KEY_HOST_IP "ip"
+    #define KEY_HOST_PORT "port"
+    #define KEY_RCODE "rcode"
+    #define KEY_RESULT "result
+
+    enum class MType {
+        REQ_RPC = 0,
+        RSP_RPC,
+        REQ_TOPIC,
+        RSP_TOPIC,
+        REQ_SERVICE,
+        RSP_SERVICE
+    };  
+
+    enum class RCode { // 响应码类型定义 
+        RCODE_OK = 0,
+        RCODE_PARSE_FAILED,
+        RCODE_ERROR_MSGTYPE,
+        RCODE_INVALID_MSG,
+        RCODE_DISCONNECTED,
+        RCODE_INVALID_PARAMS,
+        RCODE_NOT_FOUND_SERVICE,
+        RCODE_INVALID_OPTYPE,
+        RCODE_NOT_FOUND_TOPIC,
+        RCODE_INTERNAL_ERROR
+    };
+
+    static std::string errReason(RCode code) {
+        static std::unordered_map<RCode, std::string> err_map = { // 保存不同RCode对应错误原因
+            {RCode::RCODE_OK, "成功处理！"},
+            {RCode::RCODE_PARSE_FAILED, "消息解析失败！"},
+            {RCode::RCODE_ERROR_MSGTYPE, "消息类型错误！"},
+            {RCode::RCODE_INVALID_MSG, "⽆效消息"},
+            {RCode::RCODE_DISCONNECTED, "连接已断开！"},
+            {RCode::RCODE_INVALID_PARAMS, "⽆效的Rpc参数！"},
+            {RCode::RCODE_NOT_FOUND_SERVICE, "没有找到对应的服务！"},
+            {RCode::RCODE_INVALID_OPTYPE, "⽆效的操作类型"},
+            {RCode::RCODE_NOT_FOUND_TOPIC, "没有找到对应的主题！"},
+            {RCode::RCODE_INTERNAL_ERROR, "内部错误！"}
+            };
+        auto it = err_map.find(code); // 查找有没有对应错误
+        if (it == err_map.end()) return "未知错误！";
+        return it->second;
+    }
+    
+    enum class RType { //RPC请求类型定义 
+        REQ_SYNC = 0, // 同步请求：等待收到响应后返回
+        REQ_ASYNC, // 异步请求：返回异步对象，在需要的时候通过异步对象获取响应结果（还未收到结果会阻塞）
+        REQ_CALLBACK // 回调请求：设置回调函数，通过回调函数对响应进⾏处理
+    };
+
+    enum class TopicOptype { // 主题操作类型定义
+    TOPIC_CREATE = 0, // 主题创建
+    TOPIC_REMOVE, // 主题删除
+    TOPIC_SUBSCRIBE, // 主题订阅
+    TOPIC_CANCEL, // 主题取消订阅
+    TOPIC_PUBLISH // 主题消息发布
+    };
+
+    enum class ServiceOptype { // 服务操作类型定义
+        SERVICE_REGISTRY = 0, // 服务注册
+        SERVICE_DISCOVERY, // 服务发现
+        SERVICE_ONLINE, // 服务上线
+        SERVICE_OFFLINE, // 服务下线
+        SERVICE_UNKNOW
+    };
+}
