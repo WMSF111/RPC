c27117b507dcf1a9139dc8fb3786a3cc2c931c06
WMSF111 <2364169122@qq.com>
2025-10-13 16:52:05 +0800

    55.完成rpc_caller注释

--- source/client/rpc_caller.hpp
+++ source/client/rpc_caller.hpp
@@ -6,14 +6,17 @@ namespace bitrpc{
         class RpcCaller{
             public:
                 using ptr = std::shared_ptr<RpcCaller>;
-                // std::future 的 get() 方法会返回异步操作的结果。对于类型 Json::Value，它会返回 Json::Value 的一个拷贝。
-                // 因此，不需要传递引用，因为你并不需要直接访问结果的内存位置，返回的拷贝就是你操作的对象。
-                using JsonAsyncResponse = std::future<Json::Value>;
+                // 设置异步call的响应类型
+                using JsonAsyncResponse = std::future<Json::Value>; 
+                // 设置回调函数的函数模板，输入参数是Json::Value
                 using JsonResponseCallback = std::function<void(const Json::Value&)>; //注意要加上&
+                
                 RpcCaller(const Requestor::ptr &requestor) : _requestor(requestor){}
-                //requestor中的处理是针对BaseMessage进行处理的
+
+                //requestor中的send处理是针对BaseMessage进行处理的
                 //用于在rpccaller中针对结果的处理是针对 RpcResponse里边的result进行的
                 // 同步
+                // BaseConnection是一个基类，可能会有多个派生类。通过使用智能指针，可以利用多态性，传递基类指针指向派生类的对象。
                 bool call(const BaseConnection::ptr &conn, const std::string &method,
                         const Json::Value &params, Json::Value &result){
                     //1.组织请求
@@ -22,8 +25,9 @@ namespace bitrpc{
                     req_msg->setMethod(method);
                     req_msg->setMtype(MType::REQ_RPC);
                     req_msg->setParams(params);
-                    BaseMessage::ptr rsp_msg;
-                    //2.发送请求
+                    BaseMessage::ptr rsp_msg; //构造响应信息
+                    //2.服务器发送请求
+                    //send是Base级别，需要传入Base类型
                     bool ret = _requestor->send(conn, std::dynamic_pointer_cast<BaseMessage>(req_msg), rsp_msg);
                     if (ret == false) {
                         ELOG("同步Rpc请求失败！");
@@ -31,7 +35,7 @@ namespace bitrpc{
                     }
                     DLOG("收到响应，进行解析，获取结果!");
                     //3. 等待响应
-                    auto rpc_rsp_msg = std::dynamic_pointer_cast<RpcResponse>(rsp_msg);
+                    auto rpc_rsp_msg = std::dynamic_pointer_cast<RpcResponse>(rsp_msg); //响应信息转化为RPC类型
                     if (!rpc_rsp_msg) {
                         ELOG("rpc响应，向下类型转换失败！");
                         return false;
@@ -40,15 +44,14 @@ namespace bitrpc{
                         ELOG("rpc请求出错：%s", errReason(rpc_rsp_msg->rcode()));
                         return false;
                     }
-                    result = rpc_rsp_msg->result();
+                    result = rpc_rsp_msg->result(); // 设置result内容
                     DLOG("结果设置完毕！");
                     return true;
-
                 }
                 // 异步
                 bool call(const BaseConnection::ptr &conn, const std::string &method,
-                        const Json::Value &params, std::future<Json::Value> &result){
-                    // 1.构造异步回调请求。
+                        const Json::Value &params, JsonAsyncResponse &result){
+                    // 1.组织请求
                     // 设置回调函数（利用回调完成异步），回调函数中会传入一个promise对象，在回调函数中去堆promise设置数据
                     // 因为返回的时Message，用户需要Json
                     auto req_msg = MessageFactory::create<RpcRequest>();
@@ -64,9 +67,10 @@ namespace bitrpc{
                     // this：因为 Callback 是 RpcCaller 类的成员函数。 这样，当回调被调用时，它就知道是哪个对象来执行该成员函数。
                     // json_promise 必须是与 RpcCaller::Callback 的第一个参数类型一致。
                     // std::placeholders::_1：这是一个占位符，表示当调用新创建的函数对象时，调用者需要提供一个参数，这个参数将会传递给 Callback 函数的第二个参数。
+                    // 3. 关联RequestCallback回调函数
                     Requestor::RequestCallback cb = std::bind(&RpcCaller::Callback, 
                         this, json_promise, std::placeholders::_1);
-                    // 向服务器发送回调异步请求
+                    //4. 向服务器发送回调异步请求
                     bool ret = _requestor->send(conn, std::dynamic_pointer_cast<BaseMessage>(req_msg), cb);
                     if (ret == false) {
                         ELOG("异步Rpc请求失败！");
@@ -77,14 +81,16 @@ namespace bitrpc{
                 //回调
                 bool call(const BaseConnection::ptr &conn, const std::string &method,
                     const Json::Value &params, const JsonResponseCallback &cb) {
+                    // 1. 构造响应请求
                     auto req_msg = MessageFactory::create<RpcRequest>();
                     req_msg->setID(UUID::uuid());
                     req_msg->setMtype(MType::REQ_RPC);
                     req_msg->setMethod(method);
                     req_msg->setParams(params);
-
+                    // 2. 关联回调函数
                     Requestor::RequestCallback req_cb = std::bind(&RpcCaller::Callback1, 
                         this, cb, std::placeholders::_1);
+                    // 3. 服务器发送回调请求
                     bool ret = _requestor->send(conn, std::dynamic_pointer_cast<BaseMessage>(req_msg), req_cb);
                     if (ret == false) {
                         ELOG("回调Rpc请求失败！");
@@ -93,7 +99,7 @@ namespace bitrpc{
                     return true;
                 }
             private:
-                // 回调
+                // 普通回调
                 void Callback1(const JsonResponseCallback &cb, const BaseMessage::ptr &msg)  {
                     //将msg转换成RpcResponse
                     auto rpc_rsp_msg = std::dynamic_pointer_cast<RpcResponse>(msg);
